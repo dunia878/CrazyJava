d928b6c0940c7d8f007e6afe929daf494f7bf102
gengjz <gengjz@ccic.com>
2015-03-30 10:31:21 +0800

    Signed-off-by: gengjz <gengjz@ccic.com>

---
+++ eee/src/ch17/Server.java
@@ -0,0 +1,28 @@
+package ch17;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public Server() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws IOException{
+		ServerSocket ss = new ServerSocket(30000);
+		while(true){
+			Socket socket = ss.accept();
+			PrintStream ps = new PrintStream(socket.getOutputStream());
+			for (int i = 0; i < 10; i++) {
+				
+				ps.println("hello");
+			}
+			ps.close();
+			socket.close();
+		}
+	}
+
+}
